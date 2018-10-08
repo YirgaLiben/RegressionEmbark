cc7d75a287a481f768d7d77b7b7698adf32f4051
YirgaLiben <43966979+YirgaLiben@users.noreply.github.com>
2018-10-08 13:26:00 -0400

    first Commit

---
+++ RegressionE/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegressionE
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            Assert.AreEqual ("test","test",true );
+        }
+    }
+}
